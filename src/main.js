e53e6a3d41bd2d3d6fbe7a2fcc3253b144b61a42
Caviar <forworker137@gmail.com>
2024-12-07 03:57:31 +0200

    done

---
+++ src/main.js
@@ -0,0 +1,51 @@
+'use strict';
+
+import {
+  fillGalery,
+  displayAlert,
+  clearGallery,
+  toggleLoader,
+} from './js/render-functions';
+import fetchImages from './js/pixabay-api';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const form = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+const lightbox = new SimpleLightbox('.gallery a');
+const ALLERT_MSG =
+  'Sorry, there are no images matching your search query. Please try again!';
+
+function handleImages(data) {
+  if (!data.hits.length) {
+    displayAlert(ALLERT_MSG);
+  }
+  fillGalery(data.hits, gallery);
+  lightbox.refresh();
+  form.reset();
+}
+
+function loadGallery(condition) {
+  clearGallery();
+  toggleLoader();
+
+  fetchImages(condition)
+    .then(d => {
+      handleImages(d);
+    })
+    .catch(e => {
+      console.log(e);
+    })
+    .finally(() => {
+      toggleLoader();
+    });
+}
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+  const condition = form.elements.input.value;
+  loadGallery(condition);
+}
+
+form.addEventListener('submit', handleSubmit);
+loadGallery('');
